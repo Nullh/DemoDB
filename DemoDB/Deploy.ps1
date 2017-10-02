b4909033a21087581809cae8542bb0a737209e8f
Neil <neil@openprojects.co.uk>
2017-10-02 20:25:03 +0100

    Switched deploy script and nuspec

--- DemoDB/Deploy.ps1
+++ DemoDB/Deploy.ps1
@@ -1,9 +1,44 @@
-# load Dac Pac
-add-type -path "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
-
-# make DacServices object, needs a connection string 
-$d = new-object Microsoft.SqlServer.Dac.DacServices "server=(local)"
-
-# Load dacpac from file & deploy to database named pubsnew 
-$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($DacPacFile) 
-$d.Deploy($dp, $DatabaseName, $true)
+# Set params
+if (! $dbName)
+{
+    Write-Host "Missing required variable dbName" -ForegroundColor Yellow
+    exit 1
+}
+if (! $dbPublishProfile)
+{
+    Write-Host "Missing required variable dbPublishProfile" -ForegroundColor Yellow
+    exit 1
+}
+if (! $dbUser)
+{
+    Write-Host "Missing required variable dbUser" -ForegroundColor Yellow
+    exit 1
+}
+if (! $dbPassword)
+{
+    Write-Host "Missing required variable dbPassword" -ForegroundColor Yellow
+    exit 1
+}
+ 
+# Add the DLL
+# For 64-bit machines
+Add-Type -path "${Env:ProgramFiles(x86)}Microsoft SQL Server110DACbinMicrosoft.SqlServer.Dac.dll"
+ 
+# Create the connection string
+$d = New-Object Microsoft.SqlServer.Dac.DacServices ("data source=(local);User Id = " + $dbUser + ";pwd=" + $dbPassword)
+ 
+#Load the dacpac
+$dacpac = (Get-Location).Path + "ContentDatabase.dacpac"
+$dacpacoptions = (Get-Location).Path + "Content" +  $dbPublishProfile + ".publish.xml"
+ 
+Write-Host $dacpac
+Write-Host $dacpacoptions
+ 
+#Load dacpac from file & deploy to database
+$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($dacpac)
+ 
+#Read a publish profile XML to get the deployment options
+$dacProfile = [Microsoft.SqlServer.Dac.DacProfile]::Load($dacpacoptions)
+ 
+# Deploy the dacpac
+$d.Deploy($dp, $dbName, $true, $dacProfile.DeployOptions)
