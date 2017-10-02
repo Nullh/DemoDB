a221afa623cfc6e2286f30f909ea754cc7f7148c
Neil <neil@openprojects.co.uk>
2017-10-02 20:03:09 +0100

    Changed nuspec and deploy script

--- DemoDB/Deploy.ps1
+++ DemoDB/Deploy.ps1
@@ -1,40 +1,9 @@
-# Set params
-$databaseName = "Movies"
+# load Dac Pac
+add-type -path "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
 
-# Add the DLL
-# For 32-bit machines
-#Add-Type -path "C:\Program Files\Microsoft SQL Server\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
-# For 64-bit machines
-Add-Type -path "C:\Program Files\Microsoft SQL Server\130\DAC\bin\Microsoft.SqlServer.Dac.dll"
+# make DacServices object, needs a connection string 
+$d = new-object Microsoft.SqlServer.Dac.DacServices "server=(local)"
 
-# Create the connection strnig
-$d = New-Object Microsoft.SqlServer.Dac.DacServices "server=(local)"
-
-$dacpac = (Get-Location).Path + "\Content\" + $databaseName + ".dacpac"
-
-Write-Host $dacpac
-
-# Load dacpac from file & deploy to database
-$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($dacpac)
-
-# Set the DacDeployOptions
-$options = New-Object Microsoft.SqlServer.Dac.DacDeployOptions -Property @{
- 'BlockOnPossibleDataLoss' = $true;
- 'DropObjectsNotInSource' = $false;
- 'ScriptDatabaseOptions' = $true;
- 'IgnorePermissions' = $true;
- 'IgnoreRoleMembership' = $true
-}
-
-# Generate the deplopyment script
-$deployScriptName = $databaseName + ".sql"
-$deployScript = $d.GenerateDeployScript($dp, $databaseName, $options)
-
-# Return the script to the log
-Write-Host $deployScript
-
-# Write the script out to a file
-$deployScript | Out-File $deployScriptName
-
-# Deploy the dacpac
-$d.Deploy($dp, $databaseName, $true, $options)
+# Load dacpac from file & deploy to database named pubsnew 
+$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($DacPacFile) 
+$d.Deploy($dp, $DatabaseName, $true)
